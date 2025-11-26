ce9ca581c93bda71aed22941f76a32bfc2553c61
Miona <m_km29.s2@icloud.com>
2025-11-26 19:41:34 +0900

    Initial commit

---
+++ src/resources/views/login.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>FashionablyLate</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</head>
+
+<body>
+  <header class="header">
+    <div class="header__inner">
+      <div class="header-utilities">
+        <a class="header__logo" href="/">
+          FashionablyLate
+        </a>
+        <nav>
+          <ul class="header-nav">
+            <li class="header-nav__item">
+              <a class="header-nav__link" href="/register">register</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+
+  <div class="login-form__content">
+  <div class="login-form__heading">
+    <h2>ログイン</h2>
+  </div>
+  <form class="form">
+    <div class="form__group">
+      <div class="form__group-title">
+        <span class="form__label--item">メールアドレス</span>
+      </div>
+      <div class="form__group-content">
+        <div class="form__input--text">
+          <input type="email" name="email" value="{{ old('email') }}" />
+        </div>
+        <div class="form__error">
+          @error('email')
+          {{ $message }}
+          @enderror
+        </div>
+      </div>
+    </div>
+    <div class="form__group">
+      <div class="form__group-title">
+        <span class="form__label--item">パスワード</span>
+      </div>
+      <div class="form__group-content">
+        <div class="form__input--text">
+          <input type="password" name="password" />
+        </div>
+        <div class="form__error">
+          @error('password')
+          {{ $message }}
+          @enderror
+        </div>
+      </div>
+    </div>
+    <div class="form__button">
+      <button class="form__button-submit" type="submit">ログイン</button>
+    </div>
+  </form>
